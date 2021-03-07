4702019a79164194a8f644d83d9f51e65059bb77
Nardin Lachowski <nardin@live.com>
2021-03-06 23:00:25 -0500

    added to html routes file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+
 // Dependencies
 const path = require('path');
 
@@ -6,9 +7,14 @@ const path = require('path');
     res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
 
-  // blog route loads blog.html
+  // exercise route loads exercise.html
   router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
+  });
+
+  // If no matching route is found default to home
+  router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   module.exports = router;
